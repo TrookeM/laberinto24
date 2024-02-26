7a8e3e6f804fb8d16a4913ea3f7b93de17c3d18c
TrookeM <migatoyorch@gmail.com>
2024-02-26 16:35:21 +0100

    fixed test lab2Hab

--- test_laberinto.py
+++ test_laberinto.py
@@ -7,10 +7,10 @@ class TestGame(unittest.TestCase):
     def test_build_room_returns_room_with_walls(self):
         game = Game()
         room = game.build_room(1)
-        self.assertIsNotNone(room.walls[0])  # Check north wall
-        self.assertIsNotNone(room.walls[1])  # Check east wall
-        self.assertIsNotNone(room.walls[2])  # Check south wall
-        self.assertIsNotNone(room.walls[3])  # Check west wall
+        self.assertIsNotNone(room.north)  # Check north wall
+        self.assertIsNotNone(room.east)  # Check east wall
+        self.assertIsNotNone(room.south)  # Check south wall
+        self.assertIsNotNone(room.west)  # Check west wall
 
     def test_build_room_returns_room_with_correct_id(self):
         game = Game()
@@ -29,6 +29,7 @@ class TestBombedGame(unittest.TestCase):
     def test_bombed_build_room_returns_bombed_walls(self):
         game = BombedGame()
         room = game.build_room(1)
-        for wall in room.walls:
-            self.assertIsInstance(wall, BombedWall)  # Check all walls are BombedWall
-
+        self.assertIsInstance(room.north, BombedWall)
+        self.assertIsInstance(room.east, BombedWall)
+        self.assertIsInstance(room.south, BombedWall)
+        self.assertIsInstance(room.west, BombedWall)
